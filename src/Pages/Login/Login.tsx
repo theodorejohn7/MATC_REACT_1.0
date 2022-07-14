ec242eeb0f8f0b8be92921b492ce8bcd2a29a572
theodorejohn7 <stheodorejohn@hotmail.com>
2022-07-14 19:15:39 +0530

    fixed errors in register

--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,27 +1,48 @@
 import * as React from "react";
- 
-import {Container, Typography,Grid, Button} from "@mui/material"
+
+import { Container, Typography, Grid, Button } from "@mui/material";
+
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+
 import { Formik, FormikHelpers, FormikProps, Form, Field } from "formik";
 import axios from "axios";
 
-import {FormTextField} from "../../components/FormTextField/FormTextField"
-import ValidationSchema from "./ValidationSchema"
+import { FormTextField } from "../../components/FormTextField/FormTextField";
+import ValidationSchema from "./ValidationSchema";
 interface FormValues {
   name: string;
   password: string;
 }
 
- 
 export default function Login() {
-  const [username,setUsername]=React.useState("");
-  const [password,setPassword]= React.useState("");
+  const FIREBASE_URL = process.env.REACT_APP_FIREBASE_URL;
+
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const [errorMessage, setErrorMessage] = React.useState("");
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    margin: 0,
+    padding: 0,
+    p: 4,
+  };
   return (
-
-    <Container maxWidth="md" >
+    <Container maxWidth="md">
       {/* <Box mb={3} p={2}> */}
-      <div style={{  textAlign:'center'}}>
-       
-
+      <div style={{ textAlign: "center" }}>
         <Typography
           align="center"
           variant="h5"
@@ -29,82 +50,96 @@ export default function Login() {
         >
           Login Form
         </Typography>
-      {/* </Box> */}
-     
+        {/* </Box> */}
 
-      <Formik
-        initialValues={{
-          name: "",
-          password: ""
-        }}
-        validationSchema={ValidationSchema}
-        onSubmit={(
-          values: FormValues,
-          formikHelpers: FormikHelpers<FormValues>
-        ) => {
-          console.log(values);
+        <Formik
+          initialValues={{
+            name: "",
+            password: "",
+          }}
+          validationSchema={ValidationSchema}
+          onSubmit={(
+            values: FormValues,
+            formikHelpers: FormikHelpers<FormValues>
+          ) => {
+            console.log(values);
 
-          axios
-          .get(
-            `/${values.name}.json`
-          )
-          .then((response) => {
-           
-console.log(values.name,response.data.name)
-if(values.name === response.data.name)
-{
-  console.log("available name")
-}
-if(values.password !== response.data.password)
-{
-  console.log("invalid password")
-}
-console.log(username,password)
-          });
+            axios
+              .get(`${FIREBASE_URL}/${values.name}.json`)
+              .then((response) => {
+                console.log(values.name, response.data.name);
 
-          formikHelpers.setSubmitting(false);
-        }}
+                if (values.name === response.data.name) {
+                  console.log("available name");
+                }
+                if (values.password !== response.data.password) {
+                  console.log("invalid password");
+                  setErrorMessage("Username Registered Invalid password");
+                  setOpen(true);
+                }
+                console.log(username, password);
+              })
+              .catch((error_12) => {
+                setErrorMessage("Username Not Registered ");
+                setOpen(true);
+              });
 
-        
-      >
-        {(formikProps: FormikProps<FormValues>) => (
-          <Form noValidate autoComplete="off">
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
-                <Field
-                  name="name"
-                  label="Name"
-                  size="small"
-                  component={FormTextField}
-                />
+            formikHelpers.setSubmitting(false);
+          }}
+        >
+          {(formikProps: FormikProps<FormValues>) => (
+            <Form noValidate autoComplete="off">
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <Field
+                    name="name"
+                    label="Name"
+                    size="small"
+                    component={FormTextField}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    name="password"
+                    type="password"
+                    label="Password"
+                    size="small"
+                    component={FormTextField}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    type="submit"
+                    variant="outlined"
+                    size="large"
+                    color="primary"
+                    disabled={formikProps.isSubmitting}
+                  >
+                    Submit
+                  </Button>
+                </Grid>
               </Grid>
-              <Grid item xs={12}>
-                <Field
-                  name="password"
-                  type="password"
-                  label="Password"
-                  size="small"
-                  component={FormTextField}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <Button
-                  type="submit"
-                  variant="outlined"
-                  size="large"
-                  color="primary"
-                  disabled={formikProps.isSubmitting}
-                >
-                  Submit
-                </Button>
-              </Grid>
-            </Grid>
-          </Form>
-        )}
-      </Formik>
+            </Form>
+          )}
+        </Formik>
       </div>
-
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className="border border-secondary p-2 rounded" sx={style}>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            {errorMessage}
+          </Typography>
+          <div className="  float-end ">
+            <Button onClick={handleClose} variant="contained">
+              Ok
+            </Button>
+          </div>
+        </Box>
+      </Modal>
     </Container>
-
   );
 }
--- src/Pages/Register/Register.tsx
+++ src/Pages/Register/Register.tsx
@@ -1,20 +1,24 @@
 import * as React from "react";
 import { Container, Typography, Grid, Button } from "@mui/material";
 
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
 import { Formik, FormikHelpers, FormikProps, Form, Field } from "formik";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-import database from '../../axios/firebase'
+import database from "../../axios/firebase";
 import { FormTextField } from "../../components/FormTextField/FormTextField";
- 
+
 import ValidationSchema from "./ValidationSchema";
 
 interface FormValues {
   name: string;
-   userName: string;
+  userName: string;
   password: string;
   confirmPassword: string;
   eMail: string;
-  confirmEMail:string;
+  confirmEMail: string;
   address: string;
   pincode: number;
   state: string;
@@ -23,9 +27,36 @@ interface FormValues {
   securityAns: string;
 }
 
-export default function Login() {
+export default function Register() {
+  const FIREBASE_URL = process.env.REACT_APP_FIREBASE_URL;
+  const [status, setStatus] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState("");
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const navigate = useNavigate()
+
+
+  const style = {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    margin: 0,
+    padding: 0,
+    p: 4,
+  };
+
+
+  const handleRegister = () => {
+    navigate(`/home`)
+  }
   return (
-    <Container maxWidth="sm"  >
+    <Container maxWidth="sm">
       {/* <Box mb={3} p={2}> */}
       <div style={{ textAlign: "center" }}>
         <Typography
@@ -44,7 +75,7 @@ export default function Login() {
             password: "",
             confirmPassword: "",
             eMail: "",
-            confirmEMail:"",
+            confirmEMail: "",
             address: "",
             state: "",
             pincode: 0,
@@ -58,19 +89,44 @@ export default function Login() {
             formikHelpers: FormikHelpers<FormValues>
           ) => {
             console.log(values);
-            
-            database.ref(values.name).set({
-                name: values.name ,
-                userName: values.userName ,
-                password: values.password ,
-                eMail: values.eMail, 
-                address: values.address,
-                state: values.state,
-                pincode: values.pincode,
-                country: values.country,
-                securityQn: values.securityQn,
-                securityAns: values.securityAns,
-                }).catch(alert);
+
+            axios
+              .get(`${FIREBASE_URL}/${values.userName}.json`)
+              .then((response) => {
+                console.log("response", response.data);
+                console.log("value", values);
+
+                if (values.userName === response.data.userName) {
+                  setErrorMessage("Username already Registered");
+                  setOpen(true);
+                }
+              }).catch((_error)=>{
+                 console.log("logging data to db")
+                database
+                .ref(values.userName)
+                .set({
+                  name: values.name,
+                  userName: values.userName,
+                  password: values.password,
+                  eMail: values.eMail,
+                  address: values.address,
+                  state: values.state,
+                  pincode: values.pincode,
+                  country: values.country,
+                  securityQn: values.securityQn,
+                  securityAns: values.securityAns,
+                })
+                .catch(alert);
+                setErrorMessage("Details Registered Successfully");
+                setOpen(true);
+                console.log("data sent to Database")
+               setStatus(true)
+
+              });
+              
+
+          
+
             formikHelpers.setSubmitting(false);
           }}
         >
@@ -85,7 +141,7 @@ export default function Login() {
                     component={FormTextField}
                   />
                 </Grid>
-                
+
                 <Grid item xs={6}>
                   <Field
                     name="userName"
@@ -128,20 +184,19 @@ export default function Login() {
                     component={FormTextField}
                   />
                 </Grid>
-                <Grid item xs={12}    alignItems="stretch" >
+                <Grid item xs={12} alignItems="stretch">
                   <Field
                     name="address"
                     label="Address"
                     size="medium"
                     component={FormTextField}
-                    style={{ width:'95%'}}
+                    style={{ width: "95%" }}
                   />
                 </Grid>
                 <Grid item xs={6}>
                   <Field
                     name="pincode"
                     label="Pincode"
-                   
                     type="number"
                     size="small"
                     component={FormTextField}
@@ -155,7 +210,7 @@ export default function Login() {
                     component={FormTextField}
                   />
                 </Grid>
-                
+
                 <Grid item xs={6}>
                   <Field
                     name="country"
@@ -172,7 +227,7 @@ export default function Login() {
                     component={FormTextField}
                   />
                 </Grid>
-             
+
                 <Grid item xs={6}>
                   <Field
                     name="securityAns"
@@ -197,6 +252,28 @@ export default function Login() {
           )}
         </Formik>
       </div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className="border border-secondary p-2 rounded" sx={style}>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            {errorMessage}
+          </Typography>
+          <div className="  float-end ">
+            {!status&&<Button onClick={handleClose} variant="contained">
+              Ok
+            </Button>}
+{
+  status&&<Button onClick={handleRegister} variant="contained">
+ Navigate to Homepage
+</Button>
+} 
+          </div>
+        </Box>
+      </Modal>
     </Container>
   );
 }
