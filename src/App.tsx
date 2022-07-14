8f6997c9382c6c40b938e646261c85f041d01d66
theodorejohn7 <stheodorejohn@hotmail.com>
2022-07-14 10:34:36 +0530

    Signup page commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+ 
+import "./App.css";
+import { lazy, Suspense } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+const Login = lazy(() => import("./Pages/Login/Login"));
+const Home = lazy(() => import("./Pages/Home/Home"));
+const SignUp = lazy(() => import("./Pages/SignUp/SignUp"));
+const NotFound = lazy(() => import("./Pages/NotFound/NotFound"));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<Navigate replace to="/signup" />} />
+        </Routes>
+      </Suspense>
+      <h1>Welcome</h1>
     </div>
   );
 }
