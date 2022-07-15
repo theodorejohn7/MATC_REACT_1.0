85009cc19ad80b9664924a4fa36bbb2cc1ac820a
theodorejohn7 <theodorejohn.s@10decoders.in>
2022-07-16 02:09:34 +0530

    apolloconfigImportRemoved

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { ApolloProvider } from "@apollo/client";
+// import { ApolloProvider } from "@apollo/client";
 
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
