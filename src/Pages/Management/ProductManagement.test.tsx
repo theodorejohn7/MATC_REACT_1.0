b90f18c4e8a885b86263d289ed643f6254483f57
theodorejohn7 <stheodorejohn@hotmail.com>
2022-07-22 14:35:42 +0530

    first test completed

--- src/Pages/Management/ProductManagement.test.tsx
+++ src/Pages/Management/ProductManagement.test.tsx
@@ -3,9 +3,10 @@ import ProductManagement from "./ProductManagement";
 import { Provider } from "react-redux";
 
 import { store } from "../../redux/store";
- 
+import userEvent from "@testing-library/user-event";
+
 describe("Testing Product Management Page", () => {
-  test("render Register Form Title text", () => {
+  test("renders Product Management Page succesfully", () => {
     render(
       <Provider store={store}>
         <ProductManagement />
@@ -15,5 +16,44 @@ describe("Testing Product Management Page", () => {
       exact: false,
     });
     expect(RegisterText).toBeInTheDocument();
+  });
+
+  test("displays buttons for each category", () => {
+    render(
+      <Provider store={store}>
+        <ProductManagement />
+      </Provider>
+    );
+    const muttonButton = screen.getByText("Display Mutton Data", {
+      exact: false,
+    });
+    const chickenButton = screen.getByText("Display Chicken Data", {
+      exact: false,
+    });
+    const seafoodButton = screen.getByText("Display SeaFood Data", {
+      exact: false,
+    });
+    expect(muttonButton).toBeInTheDocument();
+    expect(chickenButton).toBeInTheDocument();
+    expect(seafoodButton).toBeInTheDocument();
+  });
+
+  test("displays message when UserName field has less than 8 characters", async () => {
+    render(
+      <Provider store={store}>
+        <ProductManagement />
+      </Provider>
+    );
+
+    const submitButton = screen.getByText("Display Mutton Data", {
+      exact: false,
+    });
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      const field = screen.getByTestId("DataTable");
+
+      expect(field).toBeInTheDocument();
+    });
   });
 });
