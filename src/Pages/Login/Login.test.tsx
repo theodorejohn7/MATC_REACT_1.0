9870d7ca0a1e94a9b52031966b589c55a842e2ab
theodorejohn7 <stheodorejohn@hotmail.com>
2022-07-25 10:41:02 +0530

    error in login test case fixed

--- src/Pages/Login/Login.test.tsx
+++ src/Pages/Login/Login.test.tsx
@@ -1,34 +1,55 @@
 import Login from "./Login";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { BrowserRouter as Router } from "react-router-dom";
 
 describe("Testing Login Component", () => {
   test("render Login Form Title text", () => {
-    render(<Login />);
+    render(
+      <Router>
+        <Login />
+      </Router>
+    );
     const LoginText = screen.getByText("Login Form", { exact: false });
     expect(LoginText).toBeInTheDocument();
   });
 
   test("render Name Text box", () => {
-    render(<Login />);
+    render(
+      <Router>
+        <Login />
+      </Router>
+    );
     const nameTextbox = screen.getByLabelText("Name", { exact: false });
     expect(nameTextbox).toBeInTheDocument();
   });
 
   test("render Password Text box", () => {
-    render(<Login />);
+    render(
+      <Router>
+        <Login />
+      </Router>
+    );
     const passwordTextbox = screen.getByLabelText("Password", { exact: false });
     expect(passwordTextbox).toBeInTheDocument();
   });
 
   test("render Submit button", () => {
-    render(<Login />);
+    render(
+      <Router>
+        <Login />
+      </Router>
+    );
     const submitButton = screen.getByText("Submit", { exact: false });
     expect(submitButton).toBeInTheDocument();
   });
 
   it("performs validation in the field", async () => {
-    render(<Login />);
+    render(
+      <Router>
+        <Login />
+      </Router>
+    );
     const username = screen.getByTestId("test_username");
     const submitButton = screen.getByTestId("test_submit");
 
