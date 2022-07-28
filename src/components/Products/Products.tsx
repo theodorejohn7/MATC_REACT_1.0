1c9925528382458af8870b4410115549846814ac
theodorejohn7 <stheodorejohn@hotmail.com>
2022-07-28 19:33:07 +0530

    eod commit

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -4,6 +4,8 @@ import { useState, useCallback, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Typography, Button } from "@mui/material";
 
+import { ProductCard, ProductCardButton } from "theo-components";
+
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 
@@ -11,6 +13,7 @@ import { SingleProduct } from "./SingleProduct";
 import { mongoInstance } from "../../axios/instance";
 import { getMuttonData } from "../../redux/action/MuttonAction";
 import { useUserLoginContext } from "../../context/UserLoginContext";
+import { useShoppingCart } from "../../context/ShoppingCartContext";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "react-multi-carousel/lib/styles.css";
@@ -27,13 +30,23 @@ interface IPost {
   netWeight: number;
   units?: number;
   id: number;
+  test: void;
 }
 
 const defaultPosts: IPost[] = [];
 
 const Products = () => {
   const API_URL = process.env.REACT_APP_API_URL;
-  const { setNotLoggedinPopup, notLoggedIn } = useUserLoginContext();
+
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShoppingCart();
+
+  const { setNotLoggedinPopup, isLoggedin, notLoggedIn } =
+    useUserLoginContext();
 
   const dispatch = useDispatch();
 
@@ -135,6 +148,10 @@ const Products = () => {
       });
   }, []);
 
+  const test:(id:number)=>void = (id:number) => {
+    console.log("test id",id)
+  }
+
   const handlePopupClose = () => {
     setNotLoggedinPopup();
     navigate(`/login`);
@@ -142,6 +159,38 @@ const Products = () => {
 
   return (
     <div className="App">
+      <div className="">
+        {!loading && (
+          <h3
+            className="display-1"
+            style={{ textShadow: "2px 2px 6px #171BCE", fontWeight: 350 }}
+          >
+            TEST - Mutton
+          </h3>
+        )}
+        <Carousel
+          ssr
+          partialVisible
+          itemClass="image-item"
+          responsive={responsive}
+        >
+          {muttonPosts.map((item) => (
+            // let newValue=(...item,isLoggedin);
+            // console.log(item,isLoggedin);
+            // item.getItemQuantity=getItemQuantity;
+            // console.log(item);
+            //  <SingleProduct {...item} />
+
+            <>
+            {item.test=test(item.id)}
+            {console.log(item.test)}
+              <ProductCard {...item} />
+              <ProductCardButton {...item} />
+            </>
+          ))}
+        </Carousel>
+      </div>
+
       <div className="">
         {!loading && (
           <h3
